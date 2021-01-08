740e7e1081d64d2ec999956313790aaacc1ecd89
Callan Bryant <callan.bryant@gmail.com>
2021-01-08 00:00:39 +0000

    disable uninitialised config

--- initialise-certbot.py
+++ initialise-certbot.py
@@ -3,17 +3,35 @@ import os
 import subprocess
 # enumerates domains to initialise certbot
 
+# ASSUMES ONE DOMAIN PER FILE
+
 os.chdir("/etc/nginx/conf.d")
 
-domains = set()
+domains = dict()
 
 for fp in os.listdir():
     with open(fp) as f:
         for line in f:
             if 'server_name' in line:
-                domains.add(line.split()[1].strip(';'))
+                domain = line.split()[1].strip(';')
+                domains[domain] = fp
+                break
 
-for domain in domains:
+for domain, fp in domains.items():
+    key = '/etc/letsencrypt/live/%s/fullchain.pem' % domain
+
+    if os.path.exists(key):
+        print("\033[33m%s\033[0m" % "\nSkipping existing %s..." % domain)
+        continue
+
+    # we use certonly to manage our own files instead of allowing certbot to
+    # edit them (so version control is easier)
+    # As such, before a certificate is obtained for the first time, a reference
+    # to a non-existent certificate will be in the requisite configuration
+    # file. As the certbot-nginx plugin needs to reload nginx, this will fail
+    # unless the config file is disabled temporarily.
+    os.rename(fp, fp + '.disabled')
+
     print("\033[32m%s\033[0m" % "\nInitialising %s..." % domain)
     subprocess.check_call([
         "certbot",
@@ -24,3 +42,10 @@ for domain in domains:
         "--nginx",
         "--domain", domain,
     ])
+
+    # re-enable
+    os.rename(fp + '.disabled', fp)
+
+# reload nginx, even though certbot would have done it -- it's necessary due to
+# the temporary config disabling we did.
+subprocess.check_call(['systemctl', 'reload', 'nginx'])
