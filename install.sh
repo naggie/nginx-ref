027ffb91d180eeb63ff480ddddc7b49e2966e09e
Callan Bryant <callan.bryant@gmail.com>
2021-01-26 03:18:38 +0000

    deal with changed pid file / user

--- install.sh
+++ install.sh
@@ -20,7 +20,7 @@ cd $(dirname $0)
 
 # deps
 apt-get update
-apt-get install -y nginx certbot python3-certbot-nginx openssl
+apt-get install -y nginx certbot python3-certbot-nginx openssl rename
 
 # remove specific files which are made redundant by this script
 purge {sites,modules}-{enabled,available}
@@ -31,8 +31,18 @@ purge koi-win koi-utf win-utf
 purge mime.types
 purge proxy_params scgi_params uwsgi_params fastcgi_params
 
+# if user or PID has to change, nginx needs to be stopped. It will be started
+# later on.
+if ! grep -q 'user www-data' /etc/nginx/nginx.conf || ! grep -q '/run/nginx.pid' /etc/nginx/nginx.conf; then
+    sudo systemctl stop nginx
+fi
+
 # replace main config
 cp nginx.conf /etc/nginx/nginx.conf
+
+# ensure permissions are right (if installing over the mattermost-omnibus this
+# is necessary as `nginx` user is used)
+chown -R www-data /var/{cache,log}/nginx
 
 # ensure important directories exist
 mkdir -p /etc/nginx/snippets
@@ -51,5 +61,14 @@ if [ ! -f /etc/nginx/dhparams.pem ]; then
     openssl dhparam -out /etc/nginx/dhparams.pem 2048
 fi
 
+# if nginx is not started:
+# get nginx started in a minimal state without loading any site config so that
+# certbot can proceed
+# Nginx may not be started if deliberately stopped due changing pid or user, see above.
+sudo rename s/.conf$/.conf.disabled/g /etc/nginx/conf.d/*.conf
+sudo service nginx start
+sudo rename s/.conf.disabled$/.conf/g /etc/nginx/conf.d/*.conf.disabled
+
+# note certbot may attempt to reload nginx
 sudo python3 initialise-certbot.py
-systemctl reload nginx # done implicitly before
+systemctl reload nginx
